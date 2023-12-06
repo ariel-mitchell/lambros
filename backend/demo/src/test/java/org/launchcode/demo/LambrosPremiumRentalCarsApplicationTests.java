df2406f8a3dc3d5192b00ec0ecbc34910da6c587
Timothy Nicolas <timnicolas87@gmail.com>
2023-12-06 17:06:17 -0600

    added Spring Boot backend

---
+++ backend/demo/src/test/java/org/launchcode/demo/LambrosPremiumRentalCarsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambrosPremiumRentalCarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
