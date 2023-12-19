dc40b32265940c5ade0ccb39dffaa97c9cc70b17
Timothy Nicolas <timnicolas87@gmail.com>
2023-12-19 01:43:31 -0600

    added a 404 page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,19 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Login from './pages/Login';
 import Register from './pages/Register';
-
+import NoPage from './pages/NoPage';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route index element={<Home />}/>
-          <Route path="/home" element={<Home />}/>
-          <Route path="/about" element={<About />}/>
-          <Route path="/login" element={<Login />}/>
-          <Route path="/register" element={<Register />}/>
+          <Route index element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<NoPage />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ frontend/src/pages/NoPage.js
@@ -0,0 +1,12 @@
+import Banner from "../components/Banner";
+
+function NoPage() {
+    return (
+        <>
+            <Banner/>
+            <h1>Error 404: Not Found</h1>
+        </>
+    )
+}
+
+export default NoPage;
