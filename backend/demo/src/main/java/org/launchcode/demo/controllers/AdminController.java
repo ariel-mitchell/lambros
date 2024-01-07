5e8b171f9a7a1506f95e1296bb8e2a21fd9fc748
Joshthedruid <33952609+Joshthedruid@users.noreply.github.com>
2024-01-07 14:56:03 -0600

    added basic admin forms for adding and changing cars

---
+++ backend/demo/src/main/java/org/launchcode/demo/controllers/AdminController.java
@@ -0,0 +1,42 @@
+package org.launchcode.demo.controllers;
+
+import org.launchcode.demo.data.CarRepository;
+import org.launchcode.demo.models.Car;
+import org.launchcode.demo.models.CarData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+@CrossOrigin
+@RequestMapping(value = "admin")
+public class CarController {
+    @Autowired
+    private CarRepository carRepository;
+
+    @GetMapping(value = "")
+    public ArrayList<Car> carDisplay(Model model){
+        ArrayList<Car> cars;
+        cars = CarData.findAvailableCars();
+        model.addAttribute(cars);
+
+        //placeholder strings can get swapped around as we figure out where
+        return cars;
+    }
+
+    @GetMapping(value = "add")
+    public String addCar(){
+        return("aString");
+    }
+
+    @GetMapping(value = "delete")
+    public String deleteCar(){
+        return("aString");
+    }
+}
