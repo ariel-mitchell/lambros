ca00b7804f3cb72b148bed742c488a821b75d84b
2xiix2 <122521905+2xiix2@users.noreply.github.com>
2024-01-15 09:16:35 -0600

    Frontend setup for payment processing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,35 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import StripeContainer from './components/StripeContainer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import NoPage from './pages/NoPage';
+import { useState } from 'react';
+import LamHur23 from './assets/LamHur23.png';
 
 function App() {
+  const [showItem, setShowItem] = useState(false)
   return (
+    <>
     <div>
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
           <Route path="*" element={<NoPage />} />
         </Routes>
       </BrowserRouter>
     </div>
+    <div className="App">
+      <h1>Lambros Checkout</h1>
+      {showItem ? <StripeContainer/> : <> <h3>$300.00</h3> <img src={LamHur23} alt="Blue car"/><button onClick={() => setShowItem(true)}>Rent Car</button> </>}
+    </div>
+    </>
   );
 } 
 
--- frontend/src/components/StripeContainer.js
+++ frontend/src/components/StripeContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
+import PaymentForm from './PaymentForm';
 
 const PUBLIC_KEY = "51OWwUzAqnfiPSR3BcjMrnTFa2jOQQcUuWFktRXa96MgC74hfRrZoqfu6hBkWYd8bxflicqdcx0hkaBdy25SKfDR700FZpDFh7S"
 
