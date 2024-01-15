c7def8f81d13096739baf4e5c124cf22c02319ae
2xiix2 <122521905+2xiix2@users.noreply.github.com>
2024-01-15 05:58:30 -0600

    Payment processing initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,6 +21,6 @@ function App() {
       </BrowserRouter>
     </div>
   );
-}
+} 
 
 export default App;
---
+++ frontend/src/components/StripeContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+const PUBLIC_KEY = "51OWwUzAqnfiPSR3BcjMrnTFa2jOQQcUuWFktRXa96MgC74hfRrZoqfu6hBkWYd8bxflicqdcx0hkaBdy25SKfDR700FZpDFh7S"
+
+const stripePromise = loadStripe(PUBLIC_KEY)
+
+export default function StripeContainer() {
+    return (
+        <Elements stripe ={stripePromise}>
+            <PaymentForm />
+        </Elements>
+    )
+}
