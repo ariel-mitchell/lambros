29aaff9e2fe2d38df9c278ac37d054eff6bf18b5
Joshthedruid <33952609+Joshthedruid@users.noreply.github.com>
2024-01-16 17:09:08 -0600

    Updated Threedee to Main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,4 @@
 import './App.css';
-import Banner from "./components/Banner";
-import CarTable from './components/CarTable';
-import AdminAdd from './components/admin/AdminAdd';
-import AdminUpdate from './components/admin/AdminUpdate';
-import ThreeDee from './components/ThreeDee';
-import { Canvas } from "@react-three/fiber";
-import AdminGenHash from './components/admin/AdminGenHash';
-import Car3DModel from './components/Car3DModel';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -37,36 +29,6 @@ function App() {
           <Route path="*" element={<NoPage />} />
         </Routes>
       </BrowserRouter>
-    <div className="App">
-      <Banner/>
-      <section>
-        <div style={{ width: "50vw", height: "50vh" }}>
-          <Canvas size={[`2000px`,`3000px`]} camera={{position: [10, 0, 80], rotation: [0,0,0], fov: 45}} style={{width: `100%`, height: `100%`, position: `relative` }}>
-            <pointLight position={[30, 30, 30]} />
-            <ambientLight intensity={[20]}/>
-            {/* <ThreeDee position={[0, 0, 0]} /> */}
-            <Car3DModel position={[0,0,0]} scale={[16,16,16]}/>
-          </Canvas>
-        </div>
-      </section>
-      <CarTable cars=""/>
-      <AdminAdd/>
-      <AdminUpdate/>
-      {/* <AdminGenHash/> */}
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,15 +1,29 @@
 import Banner from "../components/Banner"
 import CarTable from "../components/CarTable"
 import Welcome from "../components/Welcome";
+import { Canvas } from "@react-three/fiber";
+import Car3DModel from '../components/Car3DModel';
 
 function Home() {
 
     
     return(
         <>
-            <Banner/>
-            <Welcome/>
-            <CarTable cars=" "/>
+        <Banner/>
+        <div className="App">
+            <section>
+                <div style={{ width: "50vw", height: "50vh" }}>
+                <Canvas size={[`2000px`,`3000px`]} camera={{position: [10, 0, 80], rotation: [0,0,0], fov: 45}} style={{width: `100%`, height: `100%`, position: `relative` }}>
+                    <pointLight position={[30, 30, 30]} />
+                    <ambientLight intensity={[20]}/>
+                    {/* <ThreeDee position={[0, 0, 0]} /> */}
+                    <Car3DModel position={[0,0,0]} scale={[16,16,16]}/>
+                </Canvas>
+                </div>
+            </section>
+        </div>  
+        <Welcome/>
+        <CarTable cars=""/>
         </>
     )
 }
