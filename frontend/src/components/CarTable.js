2f175ff045e5c2b8ec7eab550e3452f5133d853b
2xiix2 <122521905+2xiix2@users.noreply.github.com>
2024-01-16 03:19:25 -0600

    Initial commit for stripe-payment

--- frontend/src/components/CarTable.js
+++ frontend/src/components/CarTable.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Axios from "axios";
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import { useNavigate } from 'react-router-dom';
 
 const CarTable = ({ cars }) => {
