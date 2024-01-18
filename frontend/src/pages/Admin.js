7de52f08274830f7dbad9bdc8e7c140d8549d710
Timothy Nicolas <timnicolas87@gmail.com>
2024-01-18 00:28:31 -0600

    styled admin page, added city state and address to rent page, styled invoice page, added logic to rent button for session

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -10,17 +10,19 @@ function Admin() {
     return (
         <>
         <Banner/>
-        <NavLink to="/admin/add">
-            <button>Add Car to Database</button>
-        </NavLink>
-        <br/>
-        <NavLink to="/admin/update">
-            <button>Update Car Info in Database</button>
-        </NavLink>
-        <br/>
-        <NavLink to="/admin/hash">
-            <button>Generate Hash for New Admin</button>
-        </NavLink>
+        <div style={{display:'flex', paddingTop:'50px', justifyContent:"space-evenly"}}>
+            <NavLink to="/admin/add">
+                <button className="btn btn-primary">Add Car to Database</button>
+            </NavLink>
+            <br/>
+            <NavLink to="/admin/update">
+                <button className="btn btn-primary">Update Car Info in Database</button>
+            </NavLink>
+            <br/>
+            <NavLink to="/admin/hash">
+                <button className="btn btn-primary">Generate Hash for New Admin</button>
+            </NavLink>
+        </div>
         <AdminCarTable/>
         </>
     )
